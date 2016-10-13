9be808f2292411f93fd21f183e681720d1563a3c
Nam hyeonuk <jwvg0425@naver.com>
2016-10-13 15:33:42 +0900

    init example project

---
+++ gameliftExample/Source/gameliftExample/gameliftExample.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "gameliftExample.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gameliftExample, "gameliftExample" );
---
+++ gameliftExample/Source/gameliftExample/gameliftExampleGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "gameliftExample.h"
+#include "gameliftExampleGameMode.h"
+
+
+
+
